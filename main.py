e1e7d51adc9bcad792bdfa51d6cdcdff8589b864
김진선 <sunny@MacBookAir-of-Kim.local>
2025-07-30 11:17:57 +0900

    RPG 게임 프로젝트 최초 업로드

---
+++ main.py
@@ -0,0 +1,15 @@
+import Hello
+
+Hello.greeting("진선")
+
+# main.py
+
+import calc
+
+a = 10
+b = 0
+
+print("덧셈:", calc.add(a, b))
+print("뺄셈:", calc.subtract(a, b))
+print("곱셈:", calc.multiply(a, b))
+print("나눗셈:", calc.divide(a, b))
